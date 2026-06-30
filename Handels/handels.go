8138fdcfc1e42ba54a532622817ce11ecfcbc4b3
anmovich <lollox12342345@gmail.com>
2026-06-30 21:52:41 +0300

    New JSON Decoder

--- Handels/handels.go
+++ Handels/handels.go
@@ -2,7 +2,7 @@ package handels
 
 import (
 	"encoding/json"
-	"io"
+
 	notifications "micron/Notifications"
 	timer "micron/Timer"
 	"net/http"
@@ -10,18 +10,12 @@ import (
 )
 
 func TimerHandler(w http.ResponseWriter, r *http.Request){
-	reqBody, err := io.ReadAll(r.Body)
-	if err != nil{
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
 	var t timer.Time
-	if err := json.Unmarshal(reqBody, &t); err != nil{
-		w.WriteHeader(http.StatusBadRequest)
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil{
+    w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	go func(t *timer.Time){
+		go func(t *timer.Time){
 		if err := notifications.SendNotificationTimer(t); err != nil{
 			w.WriteHeader(http.StatusInternalServerError)
 		} else{
