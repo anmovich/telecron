f9353d72f52ca6be87b988d1cb0c4e8aa9be08ec
anmovich <lollox12342345@gmail.com>
2026-07-01 23:19:50 +0300

    Rewrite TimerHandler RestAPI-like. Added ValidatorTimer

--- Handels/handels.go
+++ Handels/handels.go
@@ -2,32 +2,48 @@ package handels
 
 import (
 	"encoding/json"
+	"fmt"
 
 	notifications "micron/Notifications"
 	timer "micron/Timer"
 	"net/http"
-
 )
 
+//Функция для запуска таймера
 func TimerHandler(w http.ResponseWriter, r *http.Request){
+	method := r.Method
+
+	//Описание метода POST, запуск таймера
+	if method == http.MethodPost{
 	var t timer.Time
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil{
     w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
 		return
 	}
-	if !timer.CheckType(t){
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Неверный тип времени"))
+	ok, err := t.ValidatorTimer()
+	if !ok{
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
 			return
 
 	}
 		go func(t *timer.Time){
 		if err := notifications.SendNotificationTimer(t); err != nil{
-			w.WriteHeader(http.StatusBadRequest)	
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println(err)
 		  return
 		} 	
 		}(&t)
-	w.Write([]byte("Таймер запущен успешно, ждите хуйню"))
-
+		
+	hResponse, err := json.MarshalIndent(t, "", "    ")
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Не удалось перевести структуру")
+	} else{
+		w.WriteHeader(http.StatusCreated)
+		w.Write(hResponse)
+	}
+	}
 }
 
--- Timer/Timer.go
+++ Timer/Timer.go
@@ -1,15 +1,14 @@
 package timer
 
 import (
-
-
+	"errors"
 	"time"
 )
 
 type Time struct{
 	TimeType int `json:"type"`
 	UnformatDuration int `json:"duration"`
-	Duration time.Duration
+	Duration time.Duration `json:"formatDuration"`
 	Text string `json:"text"`
 }
 
@@ -23,9 +22,13 @@ func TimeConverter(tim int, typ int) (time.Duration, error){
 	return time.Duration(tim) * hui[typ -1], nil
 }
 
-func CheckType(t Time) bool{
+//Validating timer struct
+func (t Time) ValidatorTimer() (bool, error){
 	if t.TimeType < 1 || t.TimeType > 3{
-		return false
+		return false, errors.New("Incorrect time type")
 	}
-	return true
+	if t.UnformatDuration < 0{
+		return false, errors.New("Incorrect duration")
+	}
+	return true, nil
 }
